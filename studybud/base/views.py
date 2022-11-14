4c73cfadeac1f15566040dc4868467f051a7c1eb
thanh quach <thanhb.quach@gmail.com>
2022-11-13 23:17:07 -0500

    render html templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to Django')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('Welcome to Room')
+    return render(request, 'room.html')
