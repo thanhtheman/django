1461ffe5ff1597d9751fcb2b6f02a2b91748970d
thanh quach <thanhb.quach@gmail.com>
2022-11-14 17:36:08 -0500

    basic routing

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Welcome to Django')
+def projects(request):
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('This is project' + str(pk))
+    return render(request, 'projects/single_project.html')
