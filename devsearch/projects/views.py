4198e15e91c0ace53ebfbaca0e3069f1169667d4
thanh quach <thanhb.quach@gmail.com>
2022-11-15 08:59:37 -0500

    rendering data to template part 1

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Work Flow Automation',
+        'description': 'Real-world project at Peoples Group'
+    },
+    {
+        'id': '2',
+        'title': 'Django',
+        'description': 'Something useful for me to use'
+    },
+    {
+        'id': '3',
+        'title': 'API Integration on Upwork',
+        'description': 'Making some real $$$ with my programming skills'
+    }
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Thanh is a talented programmer.'
+    number = 11
+    context = {'message': msg, 'number': number}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     return render(request, 'projects/single_project.html')
