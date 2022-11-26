4b5d0e43e6d814e0d7529687a881713daab5c793
thanh quach <thanhb.quach@gmail.com>
2022-11-25 22:06:02 -0500

    home page developer completed

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    skills = models.ManyToManyField('Skill', blank=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default="profiles/user-default.png")
+    scoial_github = models.CharField(max_length=200, blank=True, null=True)
+    scoial_twitter = models.CharField(max_length=200, blank=True, null=True)
+    scoial_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    scoial_youtube = models.CharField(max_length=200, blank=True, null=True)
+    scoial_website = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
+    
+class Skill(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
