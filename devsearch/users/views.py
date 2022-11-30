909b16ef5bffbd192de8dfb1b753b3e5f67f2c2f
thanh quach <thanhb.quach@gmail.com>
2022-11-30 10:37:46 -0500

    basic user registration function

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Profile
-from projects.models import Project
-from django.contrib.auth.models import User
+# from projects.models import Project
+# from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def profiles(request):
@@ -16,6 +19,7 @@ def user_profile(request, pk):
     return render(request, 'users/user-profile.html', context)
 
 def login_user(request):
+    page ='login'
     if request.user.is_authenticated:
         redirect('profiles')
     
@@ -29,10 +33,29 @@ def login_user(request):
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username or Password is incorrect!')
+            messages.error(request,'Username or Password is incorrect!')
 
     return render(request, 'users/login_register.html')
 
 def logout_user(request):
     logout(request)
+    messages.info(request,'You have successfully logged out.')
     return redirect('login')
+
+def register_user(request):
+    page ='register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'Your account has been successfully created!')
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, "An error has occured, please check username & password requirements.")
+    context ={'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
