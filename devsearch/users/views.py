1c71cd232efb056e9bae43677f0264c5d643cfb2
thanh quach <thanhb.quach@gmail.com>
2022-12-01 21:24:54 -0500

    fix 'reverse... for ... arguments' bug

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -4,7 +4,7 @@ from .models import Profile
 # from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
@@ -44,10 +44,10 @@ def logout_user(request):
 
 def register_user(request):
     page ='register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
