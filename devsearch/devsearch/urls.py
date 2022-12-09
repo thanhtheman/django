209653fde9cdf42065ac5374158ecf807e583c3b
thanh quach <thanhb.quach@gmail.com>
2022-12-09 13:40:58 -0500

    building APIs & connect with frontend

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('users.urls')),
     path('projects/', include('projects.urls')),
     path('users/', include('users.urls')),
+    path('api/', include('api.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
